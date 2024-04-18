dd5a0f0d4ace58df2da69dcb4fac7d42aa97010e
networkSorcerer <gigattfyhig@gmail.com>
2024-04-18 15:13:35 +0900

    twitter

---
+++ projects/twitter/src/firebase.js
@@ -0,0 +1,14 @@
+
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCZ4AlIXzxOCXHUN6CT1jfTeFBmHfXSi-I",
+    authDomain: "twitter-b6ba4.firebaseapp.com",
+    projectId: "twitter-b6ba4",
+    storageBucket: "twitter-b6ba4.appspot.com",
+    messagingSenderId: "112043000532",
+    appId: "1:112043000532:web:3d84aded9e1694aed17141"
+  };
+
+
+export default firebase.initializeApp(firebaseConfig);
