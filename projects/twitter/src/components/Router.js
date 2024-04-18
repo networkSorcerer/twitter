dfca678347a544ca5cbf9bc9fae690ce3e15ee45
networkSorcerer <gigattfyhig@gmail.com>
2024-04-19 07:37:51 +0900

    twitter

---
+++ projects/twitter/src/components/Router.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { HashRouter as Router, Route, Switch} from "react-router-dom";
+
+const AppRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    return (
+        <Router>
+            <Switch>
+                <Route/>
+            </Switch>
+        </Router>
+    );
+};
+
+export default AppRouter;
--- projects/twitter/src/firebase.js
+++ projects/twitter/src/firebase.js
@@ -2,12 +2,12 @@
 import firebase from "firebase/compat/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCZ4AlIXzxOCXHUN6CT1jfTeFBmHfXSi-I",
-    authDomain: "twitter-b6ba4.firebaseapp.com",
-    projectId: "twitter-b6ba4",
-    storageBucket: "twitter-b6ba4.appspot.com",
-    messagingSenderId: "112043000532",
-    appId: "1:112043000532:web:3d84aded9e1694aed17141"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
 
