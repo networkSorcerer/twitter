9c8e76a6edf6210b322a90f67391ae0c6dd5270d
networkSorcerer <gigattfyhig@gmail.com>
2024-04-29 23:14:42 +0900

    twitter

--- projects/twitter/src/components/Router.js
+++ projects/twitter/src/components/Router.js
@@ -1,11 +1,11 @@
 
-import { HashRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import { HashRouter as Router, Route, Switch} from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
 import Profile from "routes/Profile";
 import Navigation from "./Navigation";
 
-const AppRouter = ({isLoggedIn}) => {
+const AppRouter = ({isLoggedIn, userObj}) => {
        return (
         <Router>
             {isLoggedIn && <Navigation />}
@@ -13,7 +13,7 @@ const AppRouter = ({isLoggedIn}) => {
                 {isLoggedIn ? (
                     <>
                     <Route exact path="/">
-                        <Home />
+                        <Home userObj={userObj}/>
                     </Route>
                     <Route exact path="/profile">
                         <Profile />
@@ -24,7 +24,7 @@ const AppRouter = ({isLoggedIn}) => {
                         <Auth />
                     </Route>
                 )}
-                <Redirect from="*" to="/" />
+               
             </Switch>
         </Router>
     );
