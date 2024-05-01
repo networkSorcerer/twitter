2eb64bdead1c4863769d369480c73ea86a5a586c
networkSorcerer <gigattfyhig@gmail.com>
2024-05-01 14:20:30 +0900

    twitter

--- projects/twitter/src/routes/Profile.js
+++ projects/twitter/src/routes/Profile.js
@@ -1,13 +1,28 @@
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
-const Profile = () => {
+const Profile = ({userObj}) => {
     const history = useHistory();
     
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
     }; 
+
+    const getMyNweets = async () => {
+        const nweets = await dbService
+        .collection("nweets")
+        .where("createId", "==", userObj.uid)
+        .orderBy("createAt", "asc")
+        .get();
+
+    console.log(nweets.docs.map((doc) => doc.data()));
+    };
+
+    useEffect(() => {
+        getMyNweets();
+    }, []);
 
     return (
         <>
