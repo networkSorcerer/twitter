9ed0fc707a238a300104b19c643d64b194c00c2c
networkSorcerer <gigattfyhig@gmail.com>
2024-05-03 13:26:12 +0900

    twitter

--- projects/twitter/src/components/AuthForm.js
+++ projects/twitter/src/components/AuthForm.js
@@ -31,7 +31,6 @@ const AuthForm = () => {
             } else {
                 data = await authService.signInWithEmailAndPassword(email, password);
             }
-            console.log(data);
         } catch (error) {
             setError(error.message);
         }
